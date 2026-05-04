ae6ee6c146d8cad9d61a527a38f775bcd6e86e01
ZahidRr <zahidrobbani44@gmail.com>
2026-05-04 18:05:41 +0700

    Fix Vercel read-only system

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
+// Paksa Laravel membuang semua cache dan file sementara ke folder /tmp bawaan Vercel
+$_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp';
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 require __DIR__ . '/../public/index.php';
